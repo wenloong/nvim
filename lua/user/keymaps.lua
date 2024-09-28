6901ca241f29d0be8861058ed16542ae803bc184
Clifford Ng <wenloong.code@gmail.com>
2024-09-28 02:01:36 -0400

    Initial commit of nvim config

---
+++ lua/user/keymaps.lua
@@ -0,0 +1,209 @@
+local nnoremap = require("user.keymap_utils").nnoremap
+local vnoremap = require("user.keymap_utils").vnoremap
+local inoremap = require("user.keymap_utils").inoremap
+local tnoremap = require("user.keymap_utils").tnoremap
+local xnoremap = require("user.keymap_utils").xnoremap
+
+local M = {}
+
+local TERM = os.getenv("TERM")
+
+-- Normal --
+-- Disable Space bar since it'll be used as the leader key
+-- nnoremap("<space>", "<nop>")
+
+-- Window +  better kitty navigation
+nnoremap("<C-j>", function()
+	if vim.fn.exists(":KittyNavigateDown") ~= 0 and TERM == "xterm-kitty" then
+		vim.cmd.KittyNavigateDown()
+	elseif vim.fn.exists(":NvimTmuxNavigateDown") ~= 0 then
+		vim.cmd.NvimTmuxNavigateDown()
+	else
+		vim.cmd.wincmd("j")
+	end
+end)
+
+nnoremap("<C-k>", function()
+	if vim.fn.exists(":KittyNavigateUp") ~= 0 and TERM == "xterm-kitty" then
+		vim.cmd.KittyNavigateUp()
+	elseif vim.fn.exists(":NvimTmuxNavigateUp") ~= 0 then
+		vim.cmd.NvimTmuxNavigateUp()
+	else
+		vim.cmd.wincmd("k")
+	end
+end)
+
+nnoremap("<C-l>", function()
+	if vim.fn.exists(":KittyNavigateRight") ~= 0 and TERM == "xterm-kitty" then
+		vim.cmd.KittyNavigateRight()
+	elseif vim.fn.exists(":NvimTmuxNavigateRight") ~= 0 then
+		vim.cmd.NvimTmuxNavigateRight()
+	else
+		vim.cmd.wincmd("l")
+	end
+end)
+
+nnoremap("<C-h>", function()
+	if vim.fn.exists(":KittyNavigateLeft") ~= 0 and TERM == "xterm-kitty" then
+		vim.cmd.KittyNavigateLeft()
+	elseif vim.fn.exists(":NvimTmuxNavigateLeft") ~= 0 then
+		vim.cmd.NvimTmuxNavigateLeft()
+	else
+		vim.cmd.wincmd("h")
+	end
+end)
+
+-- Swap between last two buffers
+nnoremap("<leader>'", "<C-^>", { desc = "Switch to last buffer" })
+
+-- Save with leader key
+nnoremap("<leader>w", "<cmd>w<cr>", { silent = false })
+
+-- Quit with leader key
+nnoremap("<leader>q", "<cmd>q<cr>", { silent = false })
+
+-- Save and Quit with leader key
+nnoremap("<leader>z", "<cmd>wq<cr>", { silent = false })
+
+-- Map Oil to <leader>e
+nnoremap("<leader>e", function()
+	require("oil").toggle_float()
+end)
+
+-- Center buffer while navigating
+nnoremap("<C-u>", "<C-u>zz")
+nnoremap("<C-d>", "<C-d>zz")
+nnoremap("{", "{zz")
+nnoremap("}", "}zz")
+nnoremap("N", "Nzz")
+nnoremap("n", "nzz")
+nnoremap("G", "Gzz")
+nnoremap("gg", "ggzz")
+nnoremap("<C-i>", "<C-i>zz")
+nnoremap("<C-o>", "<C-o>zz")
+nnoremap("%", "%zz")
+nnoremap("*", "*zz")
+nnoremap("#", "#zz")
+
+-- Press 'S' for quick find/replace for the word under the cursor
+nnoremap("S", function()
+	local cmd = ":%s/<C-r><C-w>/<C-r><C-w>/gI<Left><Left><Left>"
+	local keys = vim.api.nvim_replace_termcodes(cmd, true, false, true)
+	vim.api.nvim_feedkeys(keys, "n", false)
+end)
+
+-- Open Spectre for global find/replace
+nnoremap("<leader>S", function()
+	require("spectre").toggle()
+end)
+
+-- Open Spectre for global find/replace for the word under the cursor in normal mode
+-- TODO Fix, currently being overriden by Telescope
+nnoremap("<leader>sw", function()
+	require("spectre").open_visual({ select_word = true })
+end, { desc = "Search current word" })
+
+-- Press 'H', 'L' to jump to start/end of a line (first/last char)
+nnoremap("L", "$")
+nnoremap("H", "^")
+
+-- Press 'U' for redo
+nnoremap("U", "<C-r>")
+
+-- Turn off highlighted results
+nnoremap("<leader>no", "<cmd>noh<cr>")
+
+-- Diagnostics
+
+-- Goto next diagnostic of any severity
+nnoremap("]d", function()
+	vim.diagnostic.goto_next({})
+	vim.api.nvim_feedkeys("zz", "n", false)
+end)
+
+-- Goto previous diagnostic of any severity
+nnoremap("[d", function()
+	vim.diagnostic.goto_pev({})
+	vim.api.nvim_feedkeys("zz", "n", false)
+end)
+
+-- Goto next error diagnostic
+nnoremap("]e", function()
+	vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR })
+	vim.api.nvim_feedkeys("zz", "n", false)
+end)
+
+-- Goto previous error diagnostic
+nnoremap("[e", function()
+	vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR })
+	vim.api.nvim_feedkeys("zz", "n", false)
+end)
+
+-- Goto next warning diagnostic
+nnoremap("]w", function()
+	vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.WARN })
+	vim.api.nvim_feedkeys("zz", "n", false)
+end)
+
+-- Goto previous warning diagnostic
+nnoremap("[w", function()
+	vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.WARN })
+	vim.api.nvim_feedkeys("zz", "n", false)
+end)
+
+-- Map MaximizerToggle (szw/vim-maximizer) to leader-m
+nnoremap("<leader>m", ":MaximizerToggle<cr>")
+
+-- Resize split windows to be equal size
+nnoremap("<leader>=", "<C-w>=")
+
+-- Press leader f to format
+nnoremap("<leader>f", ":Format<cr>")
+
+-- Git keymaps --
+nnoremap("<leader>gb", ":Gitsigns toggle_current_line_blame<cr>")
+
+-- Map jj to <esc>
+inoremap("jj", "<esc>")
+
+-- Visual --
+-- Disable Space bar since it'll be used as the leader key
+vnoremap("<space>", "<nop>")
+
+-- Press 'H', 'L' to jump to start/end of a line (first/last char)
+vnoremap("L", "$<left>")
+vnoremap("H", "^")
+
+-- Paste without losing the contents of the register
+vnoremap("<A-j>", ":m '>+1<CR>gv=gv")
+vnoremap("<A-k>", ":m '<-2<CR>gv=gv")
+
+-- Reselect the last visual selection
+xnoremap("<<", function()
+	xnoremap("<leader>p", '"_dP')
+
+	-- Move selected text up/down in visual mode
+	vim.cmd("normal! <<")
+	vim.cmd("normal! gv")
+end)
+
+xnoremap(">>", function()
+	vim.cmd("normal! >>")
+	vim.cmd("normal! gv")
+end)
+
+-- Terminal --
+-- Enter normal mode while in a terminal
+tnoremap("<esc>", [[<C-\><C-n>]])
+tnoremap("jj", [[<C-\><C-n>]])
+
+-- Window navigation from terminal
+tnoremap("<C-h>", [[<Cmd>wincmd h<CR>]])
+tnoremap("<C-j>", [[<Cmd>wincmd j<CR>]])
+tnoremap("<C-k>", [[<Cmd>wincmd k<CR>]])
+tnoremap("<C-l>", [[<Cmd>wincmd l<CR>]])
+
+-- Reenable default <space> functionality to prevent input delay
+tnoremap("<space>", "<space>")
+
+return M
---
+++ lua/vim-options.lua
@@ -0,0 +1,24 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+vim.g.mapleader = " "
+
+-- Navigate vim panes better
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.wo.number = true
+
+-- Place a column line
+vim.opt.colorcolumn = "80"
+
+-- Enable the sign column to prevent the screen from jumping
+vim.opt.signcolumn = "yes"
+
+-- Enable access to System Clipboard
+vim.opt.clipboard = "unnamed,unnamedplus"
